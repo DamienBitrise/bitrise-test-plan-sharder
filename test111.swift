9dfea9d5c61ffd8ac1041a6f95f1c8431f763f5e
DamienBitrise <damien.murphy@bitrise.io>
2020-06-08 16:47:55 -0700

    fix &quot; when inside scheme script text

---
+++ test111.swift
@@ -0,0 +1,13 @@
+//
+//  test111.swift
+//  Tinder
+//
+//  Created by Damien Murphy on 6/8/20.
+//  Copyright © 2020 Tinder. All rights reserved.
+//
+
+import Cocoa
+
+class test111: NSObject {
+
+}
